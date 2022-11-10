93766e8d74ae1e3adb24b8e14db44f37e11523a9
Achewski <achewski@gmail.com>
2022-11-11 03:03:05 +1300

    Add eraser button and pixel size slider

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-let panel = document.querySelector("#panel");
-let btnRight = document.querySelector("#btnRight");
-let html = document.querySelector("html");
 let mouseDown = false;
+let penColor = "black";
+let html = document.querySelector("html");
+let panel = document.querySelector("#panel");
+let slider = document.querySelector("#slider");
+let btnLeft = document.querySelector("#btnLeft");
+let btnRight = document.querySelector("#btnRight");
 
-document.addEventListener("mousedown", () => {
-	mouseDown = true;
-})
-document.addEventListener("mouseup", () => {
-	mouseDown = false;
-})
+document.addEventListener("mousedown", () => mouseDown = true);
+document.addEventListener("mouseup", () => mouseDown = false);
 
+btnLeft.addEventListener(("click"), () => {(penColor === "black") ? penColor = "initial": penColor = "black"});
 
-function addPixels(rowSize) {
+let addPixels = ((rowSize) => {
 	for (let i = 0; i < rowSize; i++) {
 		let pixelRow = document.createElement("div");
-		panel.appendChild(pixelRow);
 		pixelRow.classList.add("pixelRow");
 		pixelRow.style.flex = "1 1 0";
 		pixelRow.style.display = "flex";
+		panel.appendChild(pixelRow);
 		for (let x = 0; x < rowSize; x++) {
-			let pixel = document.createElement("div");
-			pixelRow.append(pixel);
-			pixel.classList.add("pixel");
-			pixel.style.flex = "1 1 0";
-			pixel.addEventListener("mouseenter", () => {
-				if (mouseDown === true) {
-					pixel.style.backgroundColor = "black";
-				}
-			});
-			btnRight.addEventListener("click", () => {
-				pixel.style.backgroundColor = "initial";
+			let newPixel = document.createElement("div");
+			newPixel.classList.add("pixel");
+			newPixel.style.flex = "1 1 0";
+			pixelRow.append(newPixel);
+			newPixel.addEventListener("mouseenter", () => {if (mouseDown === true) {newPixel.style.backgroundColor = penColor}});
+			btnRight.addEventListener(("click"), () => {
+				newPixel.style.backgroundColor = "initial";
+				penColor = "black";
 			});
 		}
 	}
-}
+});
 
-addPixels(64);
+addPixels(slider.value);
+
+sliderLatestSetting = slider.value;
+slider.onmouseup = () => {
+	if (slider.value != sliderLatestSetting) {
+		panel.innerHTML = "";
+		sliderLatestSetting = slider.value;
+		addPixels(slider.value);
+		if (penColor === "initial") {penColor = "black"};
+	}
+};
